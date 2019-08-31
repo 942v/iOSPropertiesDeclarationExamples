62c7444c3c8259ae8e64aae5c7fad45627c0ed59
Guillermo Sáenz Urday <gsaenz@proatomicdev.com>
2019-08-31 18:43:26 -0500

    Added info link about extensions as categories

--- SettersGettersDemo/Assets/Fonts/UIFont+SGExtras.swift
+++ SettersGettersDemo/Assets/Fonts/UIFont+SGExtras.swift
@@ -11,6 +11,9 @@ import UIKit
 //////////////////////////////////////////////////////
 // @objc attribute applied here makes this swift    //
 // extension visible to all ObjC classes.           //
+// You can find more information regarding          //
+// extensions as categories here:                   //
+// https://medium.com/@topLayoutGuide/swift-custom-fonts-slightly-less-awful-f235e20027f3 //
 //////////////////////////////////////////////////////
 
 @objc public extension UIFont {
